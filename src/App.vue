c790d85522ea84d230dd12dcc6d195bb71e700e1
loclv <masamonedante@gmail.com>
2022-05-22 12:44:48 +0700

    feat: add vue-advanced-cropper

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
-<!--
-Say Hello World with Vue!
--->
+<template>
+  <div id="app">
+    <Cropper
+      class="cropper"
+      :src="img"
+      :stencil-props="{
+        aspectRatio: 10 / 12,
+      }"
+      @change="change"
+    />
+  </div>
+</template>
 
-<script setup lang="ts">
-const message = 'Hello World!'
+<script lang="ts" setup>
+import { Cropper, type Coordinates } from "vue-advanced-cropper"
+import "vue-advanced-cropper/dist/style.css"
+
+/**
+ * Photo by [Vasilis Karkalas](https://www.pexels.com/@vasilis-karkalas-155349971/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels) from Pexels
+ */
+const img = "https://images.pexels.com/photos/11301535/pexels-photo-11301535.jpeg"
+
+const change = ({ coordinates, canvas }: { coordinates: Coordinates; canvas: HTMLCanvasElement}) => {
+  console.log("🌳 ~ coordinates", coordinates)
+  console.log("🚀 ~ canvas", canvas)
+}
 </script>
 
-<template>
-  <h1>{{ message }}</h1>
-</template>
+<style scoped>
+.cropper {
+  height: 600px;
+}
+</style>
